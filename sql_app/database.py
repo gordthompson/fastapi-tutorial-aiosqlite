b47260632eb44f0d8fcfdb530e1f26b274fdbe5c
Gord Thompson <gord@gordthompson.com>
2021-11-19 07:55:31 -0700

    Use more explicit naming

--- sql_app/database.py
+++ sql_app/database.py
@@ -3,9 +3,9 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///aiosqlite_demo.db"
 
-engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=False)
+async_engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=False)
 SessionLocal = sessionmaker(
-    engine, expire_on_commit=False, class_=AsyncSession
+    async_engine, expire_on_commit=False, class_=AsyncSession
 )
 
 Base = declarative_base()
